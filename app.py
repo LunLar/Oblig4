f73374df9704cab85f73a7438b3e071c7f84bd5a
LunLar <larshenrikknapper@gmail.com>
2025-03-27 10:03:58 +0100

    Added navbar and dedicated sites

--- app.py
+++ app.py
@@ -5,9 +5,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def Hjem():
 
     return render_template('index.html')
+
+
+@app.route('/om_meg')
+def Om_meg():
+
+    return render_template('om_meg.html')
+
+
+@app.route('/python_flask')
+def Python_Flask():
+
+    return render_template('python_flask.html')
+
+
 
 
 if __name__ == '__main__':
