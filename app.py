16222de22a4dbf6b5ef0a6d7ab79fefe6c6c131d
LunLar <larshenrikknapper@gmail.com>
2025-03-27 10:52:16 +0100

    Fixed routing activ page

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def Hjem():
 
-    return render_template('index.html')
+    return render_template('index.html', active_page = 'Hjem')
 
 
 @app.route('/om_meg')
 def Om_meg():
 
-    return render_template('om_meg.html')
+    return render_template('om_meg.html', active_page = 'Om_meg')
 
 
 @app.route('/python_flask')
 def Python_Flask():
 
-    return render_template('python_flask.html')
+    return render_template('python_flask.html', active_page = 'Python_Flask')
 
 
 
